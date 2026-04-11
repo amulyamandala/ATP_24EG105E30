4eede185ee99663c680d41f1820dace6ba4a7faa
amulyamandala <amulyamandala007@gmail.com>
2026-04-11 23:27:43 +0530

    done

---
+++ week2/day4/13_restapi.js
@@ -0,0 +1,8 @@
+function sum(...a){
+  let sum=0
+  for (let am of a){
+    sum=sum+am
+  }
+console.log(sum)
+}
+sum(1,2,3)
---
+++ week5/react-app-2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="border mt-8 p-6 text-center">
+      <p>Footer Content</p>
+    </div>
+  );
+}
+
+export default Footer;
