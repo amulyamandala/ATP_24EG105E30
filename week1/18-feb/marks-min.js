26f9458d580f37baf4501037ded31d9c22b4b0d8
amulyamandala <amulyamandala007@gmail.com>
2026-05-15 15:16:05 +0530

    Update and rename

--- week1/18-feb/marks-min.js
+++ week1/18-feb/marks-min.js
@@ -1,8 +1,10 @@
 let marks=[90,78,65,98]
+//minimum marks
 let min=marks[0]
-
+//traversal
 for(let i=0;i<marks.length;i++){
     if(min>marks[i])
         min=marks[i]
 }
+//min marks 
 console.log(min)
